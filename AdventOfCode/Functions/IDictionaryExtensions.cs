cc8a09c13ebd26dd4c5dcae9dfd106d92f438dee
AlFas <AlFasGD@users.noreply.github.com>
2021-04-02 13:50:36 +0300

    Solve 2015/14

--- AdventOfCode/Functions/IDictionaryExtensions.cs
+++ AdventOfCode/Functions/IDictionaryExtensions.cs
@@ -7,5 +7,6 @@ namespace AdventOfCode.Functions
     public static class IDictionaryExtensions
     {
         public static IDictionary<TKey, bool> ToAvailabilityDictionary<TKey>(this IEnumerable<TKey> source) => source.ToDictionary(Selectors.SelfObjectReturner, _ => true);
+        public static IDictionary<TKey, TValue> ToDefaultValueDictionary<TKey, TValue>(this IEnumerable<TKey> source) => source.ToDictionary(Selectors.SelfObjectReturner, _ => default(TValue));
     }
 }
