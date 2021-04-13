2d0f43da8837b0d40e86a747884a54db96c37eaa
AlFas <AlFasGD@users.noreply.github.com>
2021-04-13 12:07:53 +0300

    More components that may or may not have been used somewhere

---
+++ AdventOfCode/Utilities/MultilineStringBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Utilities
+{
+    public class MultilineStringBuilder
+    {
+        private readonly List<StringBuilder> builders;
+
+        public int Count => builders.Count;
+
+        public MultilineStringBuilder(int lines)
+        {
+            builders = new(lines);
+            for (int i = 0; i < lines; i++)
+                builders.Add(new());
+        }
+
+        public StringBuilder this[int index] => builders[index];
+
+        public override string ToString() => string.Join("\n", builders);
+    }
+}
