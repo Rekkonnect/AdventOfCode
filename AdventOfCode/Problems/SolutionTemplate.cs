813ff318eebc2047956431f422ce1991235c30f9
Alex <kalfas.alex@gmail.com>
2021-12-02 22:53:35 +0200

    Publicize a sample solution template

---
+++ AdventOfCode/Problems/SolutionTemplate.cs
@@ -0,0 +1,22 @@
+﻿namespace AdventOfCode.Problems;
+
+public sealed class SolutionTemplate : Problem<int>
+{
+    public override int SolvePart1()
+    {
+        return -1;
+    }
+    public override int SolvePart2()
+    {
+        return -1;
+    }
+
+    protected override void LoadState()
+    {
+
+    }
+    protected override void ResetState()
+    {
+
+    }
+}
