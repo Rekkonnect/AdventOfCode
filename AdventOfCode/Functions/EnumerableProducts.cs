2e16b35852e36746022b5560db193bb0414e12d8
Alex <kalfas.alex@gmail.com>
2021-12-16 18:11:34 +0200

    Solve 2021/16

--- AdventOfCode/Functions/EnumerableProducts.cs
+++ AdventOfCode/Functions/EnumerableProducts.cs
@@ -10,4 +10,28 @@ public static class EnumerableProducts
 
         return product;
     }
+    public static uint Product(this IEnumerable<uint> source)
+    {
+        uint product = 1;
+        foreach (uint value in source)
+            product *= value;
+
+        return product;
+    }
+    public static long Product(this IEnumerable<long> source)
+    {
+        long product = 1;
+        foreach (long value in source)
+            product *= value;
+
+        return product;
+    }
+    public static ulong Product(this IEnumerable<ulong> source)
+    {
+        ulong product = 1;
+        foreach (ulong value in source)
+            product *= value;
+
+        return product;
+    }
 }
